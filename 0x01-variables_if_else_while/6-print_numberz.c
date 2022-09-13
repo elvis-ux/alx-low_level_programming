1a66d3343bfe2d2405768ef2b4b0f3a8ca05349b
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-12 18:07:27 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ * main - printing numberz
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int numz;
+	
+	for (numz = 0; numz <= 9; numz++)
+		putchar(numz);
+	putchar('\n');
+	return (0);
+}
