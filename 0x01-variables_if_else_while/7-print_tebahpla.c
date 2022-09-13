10a0e5705c23b765c8af99a14b49d75fb8309a39
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-12 18:34:04 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing reverse alpha
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char set2;
+
+	for (set2 = 'z'; set2 <= 'a'; set2++)
+		putchar(set2);
+	putchar('\n')
+		return (0);
+}
