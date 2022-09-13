e5503b8cac02df8736c31cb1b638831899624627
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-12 17:02:04 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	char start;
+
+	for (start = 'a'; start <= 'z'; start++)
+	{
+		putchar(start);
+	} putchar('\n');
+	return (0);
+}
