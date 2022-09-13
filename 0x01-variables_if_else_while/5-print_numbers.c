5785abca575b7c0e0f9466f80464cb9f8336b737
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-12 17:46:40 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - printing numbers
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= 9; num++)
+		printf(num);
+	return (0);
+}
+
