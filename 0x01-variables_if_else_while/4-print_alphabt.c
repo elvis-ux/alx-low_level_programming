9df853fcc14049ce16646aa290e368bbd4103037
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-12 17:24:46 -0700

    4-printing_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - printing alphabt
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char set;
+
+	for (set = 'a'; set <= 'z'; set++)
+	{
+		if ( set == 'e' && set == 'q')
+			continue;
+		else
+			putchar(set);
+	} putchar('\n');
+	return (0);
+}
