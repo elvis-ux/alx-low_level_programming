47779126dd1de8adece738c408c4c9026f8aeff2
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-14 14:44:08 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _islapha - check for alphabet character
+ * _islapha - checkes for alpha char
  * @c: The character to be checked
  * Return: 1 for alpha(lower or upper) and 0 for anything else
  */
