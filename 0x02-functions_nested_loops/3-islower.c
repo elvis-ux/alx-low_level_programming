b7838a452be1be5b805f800db947fe55e7166275
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-13 19:39:59 -0700

    printing_islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase letters
+ * Description: checks lowercase characters
+ * @c: character for checking
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
