7b1c692019d89d1d09739349eaba30df3ec23112
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-13 18:06:10 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: print _putchar and a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int j = 0;
+
+	while (j < 8)
+	{
+		_putchar(c[j]);
+		j++;
+	}
+	_putchar('\n')
+	return (0);
+}
