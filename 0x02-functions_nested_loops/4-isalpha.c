0bda961511e3c5389ebdc2f893dafb76601f6c8a
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-14 15:26:30 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * _islapha - checkes for alpha char
- * @c: The character to be checked
+ * _isalpha - checks for alpha char
  *
+ * @c:Checks for characters
  *
- * Return: 1 for alpha(lower or upper) and 0 for anything else
+ * Return: 1 for alpha char (lower or upper) and 0 for anything else
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
