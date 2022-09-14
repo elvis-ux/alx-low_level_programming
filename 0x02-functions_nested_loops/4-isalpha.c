f6f8c15b37f3125ff5ab0333a73efc4883c4f249
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-14 14:40:46 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,11 @@
  * @c: The character to be checked
  * Return: 1 for alpha(lower or upper) and 0 for anything else
  */
-int _islapha(int c)
+int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90 || (c >= 97 && c <= 122))
-			{
-			return (1);
-			}
-			return (0);
-			}
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
