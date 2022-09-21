82182b73da13d34e9c80a1c5e3153e064cd4bb74
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-21 16:30:23 -0700

    v2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,19 +14,11 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
-	int destlength = 0;
-	int count = 0;
-
-	while (dest[count] != '\0')
-	{
-		destlength++;
-
-		count++;
-	}
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
-		dest[destlength + j] = src[j];
+		dest[j] = src[j];
 
-	dest[destlength + j] = '\0';
+	for (; j < n; j++)
+		dest[j] = '\0';
 	return (dest);
 }
