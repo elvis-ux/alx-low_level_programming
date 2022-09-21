5d1098ee44f6d82dc75d37be2d4fe3c7cbc598de
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-21 16:47:38 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - comparing two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 if integer, +ve if s1 > s2 and -ve if s1 < s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+
+	while (i == 0)
+	{
+		if (*(s1 + j) '\0' &&  *(s2 + count2) == '\0')
+			break;
+		i = *(s1 + j) - *(s2 + j);
+
+		j++;
+	}
+	return (i);
+}
