cbc9a19dd98443840a307ff8bde168dca743445c
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-21 14:11:34 -0700

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: strings to be concatenated
+ * Return: The destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int j;
+
+	int destLength = 0;
+
+	int counter = 0;
+
+	while (dest[counter] != '\0')
+	{
+		destLngth++;
+
+		counter++;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+		dest[destLength + j] = src[j];
+
+	dest[destLength + j] = '\0';
+
+	return (dest);
+}
