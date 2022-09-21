41d51bb411d4601bf7ba533e5688058ef1a308ca
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-21 15:54:46 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - copies a string
+ * @dest: Destination string
+ * @src: strings concatinated
+ * @n: no of bytes to be copied
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int j;
+	int destlength = 0;
+	int count = 0;
+
+	while (dest[count] != '\0')
+	{
+		destlength++;
+
+		cont++;
+	}
+
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[destlength + j] = src[j];
+
+	dest[destlength + j] = '\0';
+	return (dest);
+}
