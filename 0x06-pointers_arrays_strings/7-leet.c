ef23c4cc3e7e364e16cd18da772d723b0684de0d
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-22 17:28:45 -0700

    done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *  leet - encoding a string to 1337
+ *  @s: string to be formatted
+ *
+ *  Return: formatted strings
+ */
+
+char *leet(char *s)
+{
+	char n = {65, 52, 69, 51, 79, 48, 55, 76, 49};
+
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j += 2)
+		{
+			if (s[i] == n[j] || s[i] == n[j] + 32)
+				s[i] = n[j + 1];
+		}
+	}
+	return (s);
+}
