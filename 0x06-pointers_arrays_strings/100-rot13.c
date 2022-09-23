747585547322a1b18b3bfafdd42a6debf313e065
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-22 18:00:36 -0700

    done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string
+ * @s: input string
+ * Return:  string to be encoded
+ */
+
+char *rot13(char *s)
+{
+	int c = 0;
+	int i;
+	char alpha[] = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX");
+	char rot13[] = ("nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM");
+
+	while (*(s + c) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + c) == alpha[i])
+			{
+				*(s + c) = rot13[i];
+				break;
+			}
+		}
+		c++;
+	}
+	return (s);
+}
