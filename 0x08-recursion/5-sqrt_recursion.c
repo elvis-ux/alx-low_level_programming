8845a1b8d2e45ac4106ea49d17cb62131aea26a9
elvis-ux <chukwukereelvis9@gmail.com>
2022-09-28 23:37:28 -0700

    5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,20 @@
 #include "main.h"
 
 /**
- * power_operation -nreturns the narural sqrt root
- * @n: munber
- * @a: iterator
- * Return: sqrt root or -1
- */
-
-int power_operation(int n, int a)
-{
-	if (a % (n / a) == 0)
-	{
-		if (a * (n / a) == n)
-			return (a);
-		else
-			return (-1);
-	}
-	return (0 + power_operation(n, a + 1));
-}
-
-/**
- * _sqrt_recursion - returns natural sqrt root
+ * _sqrt - returns the narural sqrt root
  * @n: number
- * Return natural sqrt root
+ * @y: buffer
+ * Return: n's square root
  */
 
-int _sqrt_recursion(int n)
+int _sqrt(int n, int y)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (0);
 	if (n == 1)
 		return (1);
-	return (power_operation(n, 2));
+	else if (y == n || n < 0)
+		return (-1);
+	else if (y * y == n)
+		return (y);
+	else
+		return (_sqrt(n, y + 1));
 }
