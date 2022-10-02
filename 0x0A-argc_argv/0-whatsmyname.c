1392febc922a984aa6008dad5dd181967bbe35ca
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-02 06:09:07 -0700

    0-whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * @argv: argument vector
  * Return: 0
  */
-void main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char **argv)
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
