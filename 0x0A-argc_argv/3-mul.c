9fc6d947538519d1b1bddbb2b5b7ffac75e51d67
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-02 06:54:30 -0700

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - multiples two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if two arguments arre supplied else 1
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b, mul;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b
+	printf("%d\n", mul);
+	return (0);
+}
