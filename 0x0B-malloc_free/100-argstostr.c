cc1a3985d0741803f3a64892c786b7994dac5411
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-03 19:11:13 -0700

    100-argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -32,6 +32,8 @@ char *argstostr(int ac, char **av)
 	char *s;
 
 	if (ac == 0 || av == NULL)
+		return (NULL);
+
 	for (; i < ac; i++, nc++)
 		nc += _strlen(av[i]);
 	s = malloc(sizeof(char) * nc + 1);
