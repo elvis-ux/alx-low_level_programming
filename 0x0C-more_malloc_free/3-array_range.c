7d94bfe13ec061a33d48514fe822459017fd8f1e
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-06 18:20:37 -0700

    3-array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,7 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * array_range - creates arrsy of integers
  * @min: minimum value of array
