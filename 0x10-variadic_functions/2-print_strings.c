6d2319bc2abdffb38130656d8dc37f12ac527552
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-16 09:33:14 -0700

    2-print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,10 +18,12 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		ptr = va_arg(ap, char *);
+
 		if (ptr)
 			printf("%s", str);
 		else
 			printf("(nil)");
+
 		if (i < n - 1)
 			if (separator)
 				printf("%s", separator);
