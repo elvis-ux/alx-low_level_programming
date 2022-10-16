8ce2ab50882ae4315c43b2b7a5ad519a5d1e5b6f
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-16 03:46:18 -0700

    100-main_opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,30 +11,26 @@
 int main(int argc, char *argv[])
 {
 	int i, memory;
-	char *arr;
+	char *op = (char *) main;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
 	memory = atoi(argv[1]);
-	if (memory <  0)
+
+	if (memory < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	arr = (char *)main;
 	for (i = 0; i < memory; i++)
 	{
-		if (i == memory - 1)
-		{
-			printf("%02hhx\n", arr[1]);
-			break;
-		}
-		printf("%2hhx\n ", arr[i]);
+		printf("%02x" op[i] & 0xFF);
+		if (i != memory - 1)
+			printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
