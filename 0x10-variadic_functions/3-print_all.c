265a3a51a133380c979e8c7c0172316a3961ef0c
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-16 13:28:49 -0700

    3-print_all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints everything
+ * @format: string format
+ * Return: void
+ */
+
+void print_all(const char * const format, ...);
+{
+	va_list ap;
+	int i = 0, j, c = 0;
+	char *ptr;
+	const char arr[] = "cifs";
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (arr[j])
+		{
+			if (arr[j] == format[i] && c)
+			{
+				printf(", ");
+				break;
+			}
+			j++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int)), c = 1;
+				break;
+			case 'i':
+				printf("%i", va_arg(ap, int)), c = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double)), c = 1;
+				break;
+			case 's':
+				ptr = va_arg(ap, char*), c = 1;
+				if (!ptr)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", ptr);
+				break;
+		}
+		i++;
+	}
+	printf("\n"), va_end(ap);
+}
