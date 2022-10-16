08cb125dfdbfe019a88305b34abbbd53bd1c6e9d
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-15 18:42:58 -0700

    100-main_opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcode of its own function
+ * @argc: num of arguments
+ * @argv: vector
+ * Return: successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, memory;
+	char *arr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	memory = atoi(argv[1]);
+	if (memory <  0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+	for (i = 0; i < memory; i++)
+	{
+		if (i == memory - 1)
+		{
+			printf("%02hhx\n", arr[1]);
+			break;
+		}
+		printf("%2hhx ", arr[i]);
+	}
+	return (0);
+}
