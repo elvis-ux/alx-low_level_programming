7e281eb75490d466c3208a6139b6efb620ab7814
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-24 20:37:42 -0700

    completed

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of linked list
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
