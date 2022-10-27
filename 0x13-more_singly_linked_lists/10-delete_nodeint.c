173e1042eaa6200961c966742a9e4e7f9cc26386
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-26 19:37:13 -0700

    done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -34,7 +34,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	temp = curt->next;
 	if (curt->next->next)
-		current->next = curt->next->next;
+		curt->next = curt->next->next;
 	else
 		curt->next = NULL;
 	free(temp);
