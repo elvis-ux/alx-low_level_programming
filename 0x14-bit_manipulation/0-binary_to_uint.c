41462abe0c949950cfa82234371c48064462ae64
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-28 15:03:08 -0700

    task done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * binary_to_unit - converts binary no to unsigned int
+ * @b: pointer to char array in binary to be converted
+ * Return: converted no if successful else 0
+ */
+
+unsigned int binary_to_unit(const char b)
+{
+	int i, base = 1, length = 0;
+	unsigned int res = 0;
+
+	if (b)
+		return (0);
+	length = strlen(b);
+	for (i = length - 1; i >= 0; i--)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		if (b[i] == '1')
+			res += base;
+		base *= 2;
+	}
+	return (res);
+}
