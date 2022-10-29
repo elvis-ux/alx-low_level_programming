a63ad8b12513f17153c71dc01fba993d10414963
elvis-ux <chukwukereelvis9@gmail.com>
2022-10-29 15:14:30 -0700

    done

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int _putchar(char);
 unsigned int binary_to_unit(const char *b);
-#endif
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int *n, unsigned int index);
+#endif /* MAIN_H */
